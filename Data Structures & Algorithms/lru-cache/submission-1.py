79afef1968eb505d35ecc4dbfdf16ede8e7252a9
Siddhant Anand <71188414+Siddhant-Anand@users.noreply.github.com>
2026-09-02 02:55:05 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,32 @@
+
+from collections import OrderedDict
+
+
+class LRUCache:
+
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        # Move key to the end = most recently used
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Update value and mark as recently used
+            self.cache.move_to_end(key)
+            self.cache[key] = value
+            return
+
+        # Add new key
+        self.cache[key] = value
+
+        # Remove least recently used
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
